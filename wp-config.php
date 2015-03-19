ca3896ca0647765891cdb928d1f114ed69cfa12d
Alex Zwir <alexzwir@gmail.com>
2015-03-19 07:30:20 -0300

    Alteração do wp-config

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'alexlearnstocode');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Bohdan1948$2808');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
